0057acb9cca96b16003861ea544dd5c4d5924271
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-11 21:14:14 +0100

    add global let for multiple and add if else conditions

--- main.js
+++ main.js
@@ -5,10 +5,25 @@ Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stamp
  - creo il loop per stampare i numeri da 1 a 100
  - imposto le condizioni per "fizz" e "buzz" */
 
+let multiple_three = 0
+let multiple_five = 0
+
 for (let i = 1 ; i < 101; i++){
-    console.log(i);
     
+    if (i % 3 === 0){
+        multiple_three++;
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0){
+        multiple_five++;
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
 }
+
+
 
 
 
