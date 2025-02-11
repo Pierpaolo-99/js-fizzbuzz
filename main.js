33464348eab2013d7107d4baf4c6364cf7ada3ea
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-11 21:34:10 +0100

    add if another global let for multiple and change position of if else condition

--- main.js
+++ main.js
@@ -2,15 +2,21 @@
 Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 /* Steps:
+ - imposto variabili globali per i multipli
  - creo il loop per stampare i numeri da 1 a 100
- - imposto le condizioni per "fizz" e "buzz" */
+ - imposto le condizioni per "fizz", "buzz" e "FizzBuzz" */
 
 let multiple_three = 0
 let multiple_five = 0
+let multiple_three_five = 0
 
 for (let i = 1 ; i < 101; i++){
-    
-    if (i % 3 === 0){
+
+    if (i % 3 === 0 && i % 5 === 0){
+        multiple_three_five++;
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0){
         multiple_three++;
         console.log("Fizz");
     }
