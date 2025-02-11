5a7047979e857177614e33b61382984932a9de60
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-11 20:25:07 +0100

    add for loop from 1 to 100

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
 /* Steps:
- - creo variabile per i numeri da 1 a 100
  - creo il loop per stampare i numeri da 1 a 100
  - imposto le condizioni per "fizz" e "buzz" */
 
-
+for (i = 1 ; i < 101; i++){
+    console.log(i);
+    
+}
 
 
 
